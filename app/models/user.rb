36621ad5025e2349c26a08de799f40749f29da3c
Joe <joe@mavenlink.com>
2017-02-22 01:32:32 -0500

    Remove extraneous methods from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -118,22 +118,16 @@ class User < ApplicationRecord
   def connected?; !stripe_user_id.nil?; end
 
   # Stripe account type checks
-  def managed?; stripe_account_type == 'managed'; end
-  def standalone?; stripe_account_type == 'standalone'; end
   def oauth?; stripe_account_type == 'oauth'; end
 
   def manager
-    # case stripe_account_type
-    #   when 'managed' then StripeManaged.new(self)
-    #   when 'standalone' then StripeStandalone.new(self)
-    #   when 'oauth' then StripeOauth.new(self)
-    # end
+    case stripe_account_type
+      when 'oauth' then StripeOauth.new(self)
+    end
   end
 
   def can_accept_charges?
     return true if oauth?
-    return true if managed? && stripe_account_status['charges_enabled']
-    return true if standalone? && stripe_account_status['charges_enabled']
     return false
   end
 
