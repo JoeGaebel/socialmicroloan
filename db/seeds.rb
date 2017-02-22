3f469e31ca02cc18e4c999e3843ef31b18f6a548
Joe <joe@mavenlink.com>
2017-02-22 17:13:46 -0500

    Make seeds based off of boat

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,8 +33,8 @@ boat_life = Campaign.create({
   description: "Rent is $2k per month. Boats? Boats are cheap. I need money to buy a boat, and live on it. I've got a good job I'll pay the loan back in no time.",
   goal_date: 2.weeks.from_now,
   repayment_length: 3,
-  interest_percent: 15,
-  goal_amount: 4000,
+  interest_percent: 5,
+  goal_amount: 3000,
   pledged_amount: 0,
   picture:  File.open("#{Rails.root}/spec/fixtures/boat.jpg")
 })
