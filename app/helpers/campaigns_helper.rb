9ef3609337974be45738bf5b6f928d1ab2a95ade
JoeGaebel <joe.gaebel@hotmail.com>
2017-02-26 00:24:29 -0500

    Add a support button at bottom of campaign description && re-style buttons

---
+++ app/helpers/campaigns_helper.rb
@@ -0,0 +1,5 @@
+module CampaignsHelper
+  def can_be_supported?(campaign)
+    !campaign.is_funded? && !current_user?(campaign.creator) && !current_user.supported_campaigns.include?(campaign)
+  end
+end
