555d0e266bd6d31929748a02fa987145d7540dee
JoeGaebel <joe.gaebel@hotmail.com>
2017-02-26 18:36:01 -0500

    Fix expired support logic

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,6 +1,7 @@
 module CampaignsHelper
   def can_be_supported?(campaign)
     !campaign.is_funded? &&
+    current_user &&
     !current_user?(campaign.creator) &&
     !current_user.supported_campaigns.include?(campaign) &&
     !campaign.expired?
