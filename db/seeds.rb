4c5df9d2e9d0b2bb2edb574e3a213e479fa13b7d
JoeGaebel <joe.gaebel@hotmail.com>
2017-03-08 18:06:56 -0500

    Create disney world example

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,37 +26,64 @@ eddie = User.create!({
 
 puts "created Eddie!"
 
-boat_life = Campaign.create!({
-  creator: joe,
-  title: 'Boat Life 2016 :sailboat::sailboat::sailboat:',
-  subtitle: "Rent in SF is crazy. I'm gonna live in a boat.",
-  description: File.read("#{Rails.root}/spec/fixtures/boatlife2016.txt"),
+family = User.create!({
+  name: 'Jon S',
+  email: 'jon@example.com',
+  password: 'password',
+  activated: true,
+  activated_at: Time.zone.now
+})
+
+disney_world = Campaign.create!({
+  creator: family,
+  title: 'Help My Family Go To Disney World',
+  subtitle: "A hurricane left our house and our savings in ruins. Please keep our kids' dreams alive.",
+  description: File.read("#{Rails.root}/spec/fixtures/disneyworld.txt"),
   goal_date: 2.weeks.from_now,
   repayment_length: 3,
   interest_percent: 5,
-  goal_amount: 3000,
-  picture:  File.open("#{Rails.root}/spec/fixtures/boat.jpg")
+  goal_amount: 5000,
+  picture:  File.open("#{Rails.root}/spec/fixtures/disneyworld.jpg")
 })
-
-puts "created Boat Life!"
 
 CampaignSupport.create!({
   user: eddie,
-  campaign: boat_life,
-  support_amount: 3000
+  campaign: disney_world,
+  support_amount: 2500
 })
 
-australia = Campaign.create!({
-  creator: joe,
-  title: 'Australia 2017',
-  subtitle: "I'm goin' to Australia",
-  description: "I'm goin to Australia!",
-  goal_date: 2.weeks.from_now,
-  repayment_length: 3,
-  interest_percent: 5,
-  goal_amount: 3000,
-  picture:  File.open("#{Rails.root}/spec/fixtures/australia.jpg")
-})
+
+# boat_life = Campaign.create!({
+#   creator: joe,
+#   title: 'Boat Life 2016 :sailboat::sailboat::sailboat:',
+#   subtitle: "Rent in SF is crazy. I'm gonna live in a boat.",
+#   description: File.read("#{Rails.root}/spec/fixtures/boatlife2016.txt"),
+#   goal_date: 2.weeks.from_now,
+#   repayment_length: 3,
+#   interest_percent: 5,
+#   goal_amount: 3000,
+#   picture:  File.open("#{Rails.root}/spec/fixtures/boat.jpg")
+# })
+
+# puts "created Boat Life!"
+#
+# CampaignSupport.create!({
+#   user: eddie,
+#   campaign: boat_life,
+#   support_amount: 3000
+# })
+
+# australia = Campaign.create!({
+#   creator: joe,
+#   title: 'Australia 2017',
+#   subtitle: "I'm goin' to Australia",
+#   description: "I'm goin to Australia!",
+#   goal_date: 2.weeks.from_now,
+#   repayment_length: 3,
+#   interest_percent: 5,
+#   goal_amount: 3000,
+#   picture:  File.open("#{Rails.root}/spec/fixtures/australia.jpg")
+# })
 
 expired = Campaign.create!({
   creator: eddie,
